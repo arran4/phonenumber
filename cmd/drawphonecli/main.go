c56da793fd880714af56f6f75e60eaa1e5c0e0cd
Arran Ubels <arran4@gmail.com>
2023-05-06 15:06:49 +1000

    It works.

--- cmd/drawphonecli/main.go
+++ cmd/drawphonecli/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/arran4/golang-wordwrap/util"
 	"log"
+	"os"
 	"phonenumber"
 )
 
 func main() {
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	fn := fs.String("outfile", "out.png", "The output filename")
+	if err := fs.Parse(os.Args); err != nil {
+		fmt.Println("Error processing args", err)
+	}
+	if !fs.Parsed() {
+		return
+	}
+	flag.Parse()
 	s := phonenumber.Numbers("Hello how are you?", phonenumber.OpIgnoreSpace, phonenumber.OpDotPauses)
 	log.Printf("'%s'", s)
-	if err := phonenumber.DrawPhone(s, "out.png"); err != nil {
+	gr, err := util.OpenFont("goregular")
+	if err != nil {
+		fmt.Println("Error processing args", err)
+		return
+	}
+	grf := util.GetFontFace(12, 180, gr)
+	if err := phonenumber.DrawPhoneWithText(s, *fn, grf); err != nil {
 		log.Printf("Error: %s", err)
 		return
 	}
+	log.Printf("Wrote: %s", *fn)
 }
--- picture.go
+++ picture.go
@@ -5,10 +5,12 @@ import (
 	_ "embed"
 	"fmt"
 	wordwrap "github.com/arran4/golang-wordwrap"
-	"github.com/arran4/golang-wordwrap/util"
 	"github.com/tdewolff/canvas"
 	"github.com/tdewolff/canvas/renderers"
+	"golang.org/x/image/font"
 	"image"
+	"image/color"
+	"image/draw"
 )
 
 var fontFamily *canvas.FontFamily
@@ -18,7 +20,7 @@ var (
 	phoneImageBytes []byte
 )
 
-func DrawPhone(s string, fn string) error {
+func DrawPhoneWithText(s string, fn string, fce font.Face) error {
 	fontFamily = canvas.NewFontFamily("times")
 	if err := fontFamily.LoadLocalFont("NimbusRoman-Regular", canvas.FontRegular); err != nil {
 		return fmt.Errorf("loading font: %w", err)
@@ -31,26 +33,25 @@ func DrawPhone(s string, fn string) error {
 	width := float64(phoneBounds.Dx())
 	height := float64(phoneBounds.Dy())
 
-	gr, err := util.OpenFont("goregular")
-	if err != nil {
-		return fmt.Errorf("font open error: %w", err)
-	}
-	grf := util.GetFontFace(16, 180, gr)
-
-	sw := wordwrap.NewSimpleWrapper(s, grf)
+	sw := wordwrap.NewSimpleWrapper(s, fce, wordwrap.HorizontalCenterLines)
 
 	ls, pt, err := sw.TextToRect(phoneBounds, wordwrap.FitterIgnoreY{})
 	height += float64(pt.Y)
 
-	c := canvas.New(width, height)
-	ctx := canvas.NewContext(c)
-
-	ctx.DrawImage(0, height-float64(phoneBounds.Max.Y), phoneImage, canvas.DefaultResolution)
-
 	i := image.NewRGBA(image.Rect(0, 0, int(width), int(height)-phoneBounds.Max.Y))
+	draw.Draw(i, i.Bounds(), image.White, image.Pt(0, 0), draw.Over)
 	if err := sw.RenderLines(i, ls, i.Bounds().Min); err != nil {
 		return fmt.Errorf("")
 	}
+
+	c := canvas.New(width, height)
+	ctx := canvas.NewContext(c)
+
+	ctx.SetFillColor(color.White)
+	ctx.Fill()
+
+	ctx.DrawImage((width-float64(i.Bounds().Dx()))/2, float64(phoneBounds.Dy()), i, canvas.Resolution(1))
+	ctx.DrawImage((width-float64(phoneBounds.Dx()))/2, 0, phoneImage, canvas.Resolution(1))
 
 	if err := renderers.Write(fn, c); err != nil {
 		return fmt.Errorf("writing file %s: %w", fn, err)
